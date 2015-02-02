b19e3383d1ef4ea8ba86dfaff6b47b4a25a337a1
Huskarhero <stevenjobs207@gmail.com>
2015-02-02 11:02:40 +0100

    Added entry points for restframework

--- docs/samples/shopmodels.py
+++ docs/samples/shopmodels.py
@@ -1,45 +1,25 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
-from shop.util.fields import CurrencyField
+from shop.models.deferred import ForeignKeyBuilder
+ForeignKeyBuilder.app_label = 'myshop'
 from shop.models import cart, order, product
 
 
 class Cart(cart.BaseCart):
-    class Meta:
-        app_label = 'myshop'
+    pass
 
 
 class CartItem(cart.BaseCartItem):
-    class Meta:
-        app_label = 'myshop'
+    pass
 
 
 class Order(order.BaseOrder):
-    class Meta:
-        app_label = 'myshop'
+    pass
 
 
 class OrderItem(order.BaseOrderItem):
-    class Meta:
-        app_label = 'myshop'
-
-
-class OrderExtraRow(order.BaseOrderExtraRow):
-    class Meta:
-        app_label = 'myshop'
-
-
-class OrderItemExtraRow(order.BaseItemExtraRow):
-    class Meta:
-        app_label = 'myshop'
+    pass
 
 
 class Product(product.BaseProduct):
     class Meta:
         app_label = 'myshop'
-
-    product_code = models.CharField(max_length=255, verbose_name=_("Product code"))
-    unit_price = CurrencyField(verbose_name=_("Unit price"))
-    active = models.BooleanField(default=False, verbose_name=_("Availability of product"))
