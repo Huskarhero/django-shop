751fd96a813ba972771f30bd58e97c4f2c51ef6e
Huskarhero <stevenjobs207@gmail.com>
2015-10-01 13:55:33 +0200

    safty commit before removing caption_notfound

--- shop/cascade/order.py
+++ shop/cascade/order.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.template import Template
 from django.template.loader import select_template
 from django.utils.translation import ugettext_lazy as _
 from cms.plugin_pool import plugin_pool
@@ -18,17 +17,19 @@ class ShopOrderViewsPlugin(ShopPluginBase):
         many = context.get('many')
         if many is True:
             # render Order List View
-            return select_template([
+            template_names = [
                 '{}/order/list.html'.format(shop_settings.APP_LABEL),
                 'shop/order/list.html',
-            ])
-        if many is False:
+            ]
+        elif many is False:
             # render Order Detail View
-            return select_template([
+            template_names = [
                 '{}/order/detail.html'.format(shop_settings.APP_LABEL),
                 'shop/order/detail.html',
-            ])
-        # can happen, if this plugin is abused outside of an OrderView
-        return Template('<!-- Order Views Plugin used outside of a page with a CMS app “View Order” -->')
+            ]
+        else:
+            # can happen, if this plugin is abused outside of an OrderView
+            template_names = ['cascade/generic/naked.html']
+        return select_template(template_names)
 
 plugin_pool.register_plugin(ShopOrderViewsPlugin)
