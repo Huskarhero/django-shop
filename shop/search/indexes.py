2add752fbf7736b925045d7e77c87d6228a79fee
Huskarhero <stevenjobs207@gmail.com>
2015-12-21 12:02:28 +0100

    updated docstring

--- shop/search/indexes.py
+++ shop/search/indexes.py
@@ -16,6 +16,7 @@ class ProductIndex(indexes.SearchIndex):
     text = indexes.CharField(document=True, use_template=True)
     autocomplete = indexes.EdgeNgramField(use_template=True)
     name = indexes.CharField(stored=True, indexed=False)
+    body = indexes.CharField(stored=True, indexed=False)
     product_url = indexes.CharField(stored=True, indexed=False, model_attr='get_absolute_url')
 
     def prepare(self, product):
@@ -32,6 +33,9 @@ class ProductIndex(indexes.SearchIndex):
         Retrieve name though correct translation
         """
         return product.product_name
+
+    def prepare_body(self, product):
+        return self.render_html(product, 'body')
 
     def render_html(self, product, postfix):
         """
@@ -55,4 +59,4 @@ class ProductIndex(indexes.SearchIndex):
             self.language = using
         else:
             self.language = settings.LANGUAGE_CODE
-        return self.get_model().objects.filter(active=True)
+        return self.get_model().objects.indexable()
