8d76eb7b47ca80d76d5a2feb968fd70648b93dba
Huskarhero <stevenjobs207@gmail.com>
2015-12-30 18:15:34 +0100

    Do not display language choser, if there is only one

--- example/myshop/migrations_polymorphic/0001_initial.py
+++ example/myshop/migrations_polymorphic/0001_initial.py
@@ -18,9 +18,9 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('email_auth', '0002_auto_20151011_1652'),
-        ('cms', '0013_urlconfrevision'),
-        ('filer', '0002_auto_20150606_2003'),
         ('contenttypes', '0001_initial'),
+        ('filer', '0002_auto_20150606_2003'),
+        ('cms', '0012_auto_20150607_2207'),
     ]
 
     operations = [
@@ -84,7 +84,33 @@ class Migration(migrations.Migration):
             name='Manufacturer',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+<<<<<<< HEAD:example/myshop/migrations/0001_initial.py
+                ('language_code', models.CharField(max_length=15, verbose_name='Language', db_index=True)),
+                ('tag', models.CharField(help_text='A tag to describe the property of this commodity.', max_length=255, verbose_name='Tag')),
+                ('search_indices', models.CharField(help_text='Search Indices for describing this property tag', max_length=255, null=True, verbose_name='Search Indices', blank=True)),
+                ('master', models.ForeignKey(related_name='translations', editable=False, to='myshop.CommodityTag', null=True)),
+            ],
+            options={
+                'managed': True,
+                'db_table': 'myshop_commoditytag_translation',
+                'db_tablespace': '',
+                'default_permissions': (),
+                'verbose_name': 'Commodity Tag Translation',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Customer',
+            fields=[
+                ('user', models.OneToOneField(primary_key=True, serialize=False, to=settings.AUTH_USER_MODEL)),
+                ('recognized', models.PositiveSmallIntegerField(default=0, help_text='Designates the state the customer is recognized as.', verbose_name='Recognized as', choices=[(0, 'Unrecognized'), (1, 'Guest'), (2, 'Registered')])),
+                ('salutation', models.CharField(max_length=5, verbose_name='Salutation', choices=[('mrs', 'Mrs.'), ('mr', 'Mr.'), ('na', '(n/a)')])),
+                ('last_access', models.DateTimeField(default=django.utils.timezone.now, verbose_name='Last accessed')),
+                ('extra', jsonfield.fields.JSONField(default={}, verbose_name='Extra information about this customer', editable=False)),
+                ('number', models.PositiveIntegerField(default=None, unique=True, null=True, verbose_name='Customer Number')),
+=======
                 ('name', models.CharField(max_length=50, verbose_name='Name')),
+>>>>>>> b89673f5f6effd536d7dddaf4c2543aec66846a8:example/myshop/migrations_polymorphic/0001_initial.py
             ],
             options={
             },
@@ -105,6 +131,7 @@ class Migration(migrations.Migration):
                 ('number', models.PositiveIntegerField(default=None, unique=True, null=True, verbose_name='Order Number')),
                 ('shipping_address_text', models.TextField(help_text='Shipping address at the moment of purchase.', null=True, verbose_name='Shipping Address', blank=True)),
                 ('billing_address_text', models.TextField(help_text='Billing address at the moment of purchase.', null=True, verbose_name='Billing Address', blank=True)),
+                ('number', models.PositiveIntegerField(default=None, unique=True, null=True, verbose_name='Order Number')),
                 ('customer', models.ForeignKey(related_name='orders', verbose_name='Customer', to='myshop.Customer')),
             ],
             options={
