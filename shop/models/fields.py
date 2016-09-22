10a56aae9e1a761e004f4d2dee816ea16b16e78a
Huskarhero <stevenjobs207@gmail.com>
2016-09-22 15:57:56 +0200

    check for product_name also via class, not only instance

--- shop/models/fields.py
+++ shop/models/fields.py
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.conf import settings
+from django.db import connection
 
+from shop.apps import get_tuple_version
 
-if settings.DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql':
-    from django.contrib.postgres.fields import JSONField as _JSONField
-else:
+try:
+    if str(connection.vendor) == 'postgresql':
+        import psycopg2
+
+        psycopg2_version = get_tuple_version(psycopg2.__version__[:5])
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT version()")
+            row = cursor.fetchone()[:17]
+        postgres_version = get_tuple_version(str(row[0][:17].split(' ')[1]))
+        # To be able to use the Django version of JSONField, it requires to have
+        # PostgreSQL ≥ 9.4 and psycopg2 ≥ 2.5.4, otherwise some issues could be faced.
+        if (psycopg2_version) >= (2, 5, 4) and (postgres_version >= (9, 4)):
+            from django.contrib.postgres.fields import JSONField as _JSONField
+        else:
+            raise ImportError
+    else:
+        raise ImportError
+
+except ImportError:
     from jsonfield.fields import JSONField as _JSONField
 
 
