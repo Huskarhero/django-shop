65fc933d27b6da3a256f3c94db5d4e476a8fa4c4
Huskarhero <stevenjobs207@gmail.com>
2017-09-12 16:00:31 +0200

    3rd attempt to fix #650

--- shop/migrations/0007_notification_recipient.py
+++ shop/migrations/0007_notification_recipient.py
@@ -19,7 +19,7 @@ def forwards(apps, schema_editor):
         if row.mail_to == 0:
             row.mail_to = None
             row.notify = Notify.CUSTOMER
-        row.save(update_fields=['notify'])
+        row.save(update_fields=['mail_to', 'notify'])
 
 
 def backwards(apps, schema_editor):
