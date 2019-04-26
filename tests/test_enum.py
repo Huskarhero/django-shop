94a24f0e8ca98fb2d858ba44d82fd9d42c779b93
Huskarhero <stevenjobs207@gmail.com>
2019-04-26 10:58:23 +0200

    Fix test for string based ChoiceEnum

--- tests/test_enum.py
+++ tests/test_enum.py
@@ -7,8 +7,13 @@ from shop.models.fields import ChoiceEnum, ChoiceEnumField
 
 
 class MyChoices(ChoiceEnum):
-    A = 0
-    B = 1
+    A = 0, "My choice A"
+    B = 1, "My choice B"
+
+
+class MyColor(ChoiceEnum):
+    RED = '#ff0000', "Pure red"
+    BLUE = '#0000ff', "Pure blue"
 
 
 class MyModel(models.Model):
@@ -19,15 +24,27 @@ class MyModel(models.Model):
         managed = False
 
 
-def test_enum():
+def test_int_enum():
     choice_a = MyChoices.A
     assert isinstance(choice_a, MyChoices)
     assert MyChoices.B.name == 'B'
     assert MyChoices.B.value == 1
+    assert MyChoices.B.label == "My choice B"
     choice_b = MyChoices('B')
-    assert str(choice_b) == 'MyChoices.B'
-    assert MyChoices.default() == MyChoices.A
-    assert MyChoices.choices() == [(0, 'MyChoices.A'), (1, 'MyChoices.B')]
+    assert str(choice_b) == "My choice B"
+    assert MyChoices.default == MyChoices.A
+    assert MyChoices.choices == [(0, "My choice A"), (1, "My choice B")]
+
+
+def test_str_enum():
+    red = MyColor.RED
+    assert isinstance(red, MyColor)
+    assert MyColor.BLUE.name == 'BLUE'
+    assert MyColor.BLUE.value == '#0000ff'
+    assert MyColor.BLUE.label == "Pure blue"
+    assert MyColor.BLUE == MyColor('#0000ff')
+    assert str(MyColor.BLUE) == "Pure blue"
+    assert MyColor.choices == [('#ff0000', "Pure red"), ('#0000ff', "Pure blue")]
 
 
 def test_to_python():
